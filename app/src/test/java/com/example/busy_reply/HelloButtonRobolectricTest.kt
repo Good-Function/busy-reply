4c0ebf6bc560fbc8a32475ef7fd0ead48bc618ff
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2026-01-23 17:03:51 +0100

    Remove obsolete files

--- app/src/test/java/com/example/busy_reply/HelloButtonRobolectricTest.kt
+++ app/src/test/java/com/example/busy_reply/HelloButtonRobolectricTest.kt
@@ -9,17 +9,6 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-/**
- * TDD Lesson: Testing with Robolectric
- * 
- * Robolectric runs tests on JVM (your computer) - super fast!
- * - No emulator needed
- * - Runs in seconds
- * - Perfect for TDD workflow
- * 
- * Note: Robolectric + Compose can be tricky. If this doesn't work,
- * use the instrumented test in androidTest folder instead.
- */
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [33])
 class HelloButtonRobolectricTest {
